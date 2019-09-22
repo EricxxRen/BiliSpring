7e0da0678e5b26202a37237d7a51d6acfd9c0a73
Eric Ren <ericzjhz@gmail.com>
2019-09-21 22:25:18 -0400

    initial commit

---
+++ src/main/java/com/rxx/coffeejourney/CoffeejourneyApplication.java
@@ -0,0 +1,13 @@
+package com.rxx.coffeejourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeejourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeejourneyApplication.class, args);
+    }
+
+}
