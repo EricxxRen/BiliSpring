b7e3bec92217c52b5e68150686101948afe92038
Eric Ren <ericzjhz@gmail.com>
2019-10-04 12:27:27 -0400

    modified controller

--- src/main/java/com/rxx/coffeejourney/Controller/HelloWorld.java
+++ src/main/java/com/rxx/coffeejourney/Controller/HelloWorld.java
@@ -3,11 +3,16 @@ package com.rxx.coffeejourney.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@ResponseBody表示这个类的所有方法返回的数据直接写给浏览器（如果是对象可以转为json数据）
+//@ResponseBody
+//@Controller
+
+//@RestController包含了@ResponseBody和@Controller
+@RestController
 public class HelloWorld {
 
-    @ResponseBody
     @RequestMapping("/hello")
     public String hello () {
         return "Hello World";
