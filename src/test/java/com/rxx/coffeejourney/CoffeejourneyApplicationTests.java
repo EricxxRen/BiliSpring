ae4b19c0b0e4f8d79c62edf288b825bb589c79da
Eric Ren <ericzjhz@gmail.com>
2019-10-08 22:29:16 -0400

    .yml file modification

--- src/test/java/com/rxx/coffeejourney/CoffeejourneyApplicationTests.java
+++ src/test/java/com/rxx/coffeejourney/CoffeejourneyApplicationTests.java
@@ -3,6 +3,8 @@ package com.rxx.coffeejourney;
 import com.rxx.coffeejourney.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -22,6 +24,9 @@ public class CoffeejourneyApplicationTests {
     @Autowired
     ApplicationContext ioc;
 
+    //记录器
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void testHelloService () {
         boolean b = ioc.containsBean("helloService");
@@ -31,6 +36,20 @@ public class CoffeejourneyApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println(person);
+    }
+
+    @Test
+    public void loggerTest() {
+
+        //日志级别，由低到高，trace<debug<info<warn<error
+        //可以调整需要输出的日志级别
+        logger.trace("这是trace日志...");
+        logger.debug("这是debug日志...");
+        //SpringBoot默认的显示日志级别是info及以后的日志, 没有设定
+        logger.info("这是info日志");
+        logger.warn("这是warn日志");
+        logger.error("这是error日志");
+
     }
 
 }
